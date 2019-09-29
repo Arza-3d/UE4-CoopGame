e31b7ad19c189330eb7062b0547e98b4311e6e94
Forest Guhareza <arza.3d@gmail.com>
2019-09-30 02:23:43 +0700

    jump challange complete

---
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -0,0 +1,27 @@
+#include "..\Public\SWeapon.h"
+
+// Sets default values
+ASWeapon::ASWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	MeshComp->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void ASWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
